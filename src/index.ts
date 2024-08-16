4789b770a70295588719c08a426bc00942264085
Yuto Terada <66758394+indigo-san@users.noreply.github.com>
2024-08-16 05:51:48 +0000

    stop-syncの動作、ミュート設定コマンドを実装

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {
   ChannelType,
   Client,
   GatewayIntentBits,
+  type Interaction,
   Partials,
   PermissionFlagsBits,
   SlashCommandBuilder
@@ -44,22 +45,63 @@ const listenerClient = new Client({
   partials: [Partials.Channel],
 });
 
-client.once('ready', () => {
+client.once('ready', async () => {
+  if (!client.application) return;
   const startCommand = new SlashCommandBuilder()
     .setName('start-sync')
-    .setDescription('参戦チャンネルの音声を観戦チャンネルに流します')
+    .setDescription('参戦チャンネルの中継を開始します')
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
   const stopCommand = new SlashCommandBuilder()
     .setName('stop-sync')
-    .setDescription('参戦チャンネルの音声ストップ')
+    .setDescription('参戦チャンネルの中継を停止します')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
+  const muteCommand = new SlashCommandBuilder()
+    .setName('mute-all-players')
+    .setDescription('参戦チャンネルのメンバーをミュートにします')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
+  const unmuteCommand = new SlashCommandBuilder()
+    .setName('unmute-all-players')
+    .setDescription('参戦チャンネルのメンバーのミュートを解除します')
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
 
-  client.application?.commands.set([startCommand, stopCommand], process.env.GUILD_ID as string);
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const promises: Promise<any>[] = [];
+  const commands = await client.application.commands.fetch({ guildId: process.env.GUILD_ID as string });
+  const addCommands = [startCommand, stopCommand, muteCommand, unmuteCommand];
+
+  for (const add of addCommands) {
+    const existingCommand = commands.find((v) => v.name === add.name);
+    if (existingCommand) {
+      promises.push(existingCommand.edit(add));
+    } else {
+      promises.push(client.application.commands.create(add));
+    }
+  }
+
+  await Promise.all(promises);
 });
+
+async function setMuteAllPlayers(interaction: Interaction, mute: boolean) {
+  const channel = await interaction.guild?.channels.fetch(process.env.PLAYING_CHANNEL_ID as string);
+  if (channel?.isVoiceBased()) {
+    for (const member of channel.members) {
+      if (member[1].user.bot) continue;
+      await member[1].voice.setMute(mute);
+    }
+  }
+}
+
+let abortController: AbortController | null = null;
 
 client.on('interactionCreate', async (interaction) => {
   if (interaction.isChatInputCommand()) {
     if (interaction.commandName === 'start-sync') {
+      if (abortController) {
+        await interaction.reply('既にVCの中継が開始されています');
+        return;
+      }
+
+      abortController = new AbortController();
       const watchConnection = joinVoiceChannel({
         group: 'speaker',
         channelId: process.env.WATCHING_CHANNEL_ID as string,
@@ -134,9 +176,40 @@ client.on('interactionCreate', async (interaction) => {
         });
       });
       await interaction.reply('VCを中継を開始しました');
+
+      abortController.signal.addEventListener('abort', () => {
+        playConnection.destroy();
+        watchConnection.destroy();
+      });
     } else if (interaction.commandName === 'stop-sync') {
+      if (abortController != null) {
+        abortController.abort();
+        abortController = null;
+      }
+      await interaction.reply('VCの中継を停止しました');
+    } else if (interaction.commandName === 'mute-all-players') {
+      await setMuteAllPlayers(interaction, true);
+      await interaction.reply('ミュートを設定しました');
+    } else if (interaction.commandName === 'unmute-all-players') {
+      await setMuteAllPlayers(interaction, false);
+      await interaction.reply('ミュートを設定解除しました');
     }
   }
+});
+client.on('shardDisconnect', () => {
+  if (abortController != null) {
+    abortController.abort();
+    abortController = null;
+  }
+});
+process.on('SIGINT', () => {
+  if (abortController != null) {
+    abortController.abort();
+    abortController = null;
+  }
+  client.destroy();
+  listenerClient.destroy();
+  process.exit();
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
